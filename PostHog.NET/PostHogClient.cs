01fefaf4a822d26a82bf00bb8bee5d140e5c2bef
maxidea1024@gmail.com <maxidea1024@gmail.com>
2024-11-06 17:31:10 +0900

    Update PostHogClient.cs

--- PostHog.NET/PostHogClient.cs
+++ PostHog.NET/PostHogClient.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 
 namespace PostHog
 {
+    // TODO: 메모리 풀링
     public class PostHogClient : IPostHogClient
     {
         private readonly AsyncIntervalFlushHandler _flushHandler;
